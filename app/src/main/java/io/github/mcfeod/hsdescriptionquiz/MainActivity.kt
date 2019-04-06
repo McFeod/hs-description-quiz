c7f97f9eb9251bc791b9f3c82ab94d32f31092f7
Georgy Fedoseyev <fedoseyevgi@yandex.ru>
2019-04-06 16:45:33 +0500

    Markup for settings activity

--- app/src/main/java/io/github/mcfeod/hsdescriptionquiz/MainActivity.kt
+++ app/src/main/java/io/github/mcfeod/hsdescriptionquiz/MainActivity.kt
@@ -1,15 +1,19 @@
 package io.github.mcfeod.hsdescriptionquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import kotlinx.android.synthetic.main.activity_main.settingsButton
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
 
+        settingsButton.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/io/github/mcfeod/hsdescriptionquiz/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package io.github.mcfeod.hsdescriptionquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
